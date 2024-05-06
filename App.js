2dee9fc1b80c66fa6b9e6be58f9052962f2464cc
Emilly12321 <emillysilvaluz2839@gmail.com>
2024-05-06 16:46:16 -0300

    Tela

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
-import AppNavigator from "./src/navigation/AppNavigator";
-
-export default function App() {
-  return <AppNavigator />;
-}
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignIn from "./src/screens/SignIn";
+import SignUp from "./src/screens/SignUp";
+import { Provider } from "./src/Context/authContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default () => {
+  return (
+    <Provider>
+      <SafeAreaProvider>
+        <App />
+      </SafeAreaProvider>
+    </Provider>
+  );
+};
